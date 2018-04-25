1af3bee3b22ba1763a7bfdecae3c128b99b36c75
Baptiste Velado <bvelado@apperture.fr>
2018-04-25 14:22:50 +0200

    Added TextMeshPro and UI code for a clock

---
+++ GitSM_Unity_Main/Assets/Sources/UI/NiceClockUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NiceClockUI : MonoBehaviour {
+
+    private TextMeshProUGUI _text;
+
+    private void Awake () {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+	
+	private void Update () {
+        _text.text = string.Format("{0}:{1}", Clock.GetHours(), Clock.GetMinutes());
+	}
+}
